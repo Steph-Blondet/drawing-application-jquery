a079de67a6e15d381c16b9c0655192250d632744
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2017-02-13 22:33:39 -0500

    drawing board working

--- js/app.js
+++ js/app.js
@@ -2,7 +2,10 @@
 //Solution: When user interacts cause change properly.
 
 var color = $(".selected").css("background-color");
-var context = $("canvas")[0].getContext("2d");
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
 
 //When clicking on control list items
 $(".controls").on("click", "li", function(){
@@ -47,11 +50,19 @@ $("#addNewColor").click(function(){
 
 
 //On mouse events on the canvas
+$canvas.mousedown(function(e){
+    lastEvent = e;
+    mouseDown = true;
+}).mousemove(function(e){
     //Draw lines
-    context.beginPath();
-    context.moveTo(10, 10);
-    context.lineTo(20, 10);
-    context.lineTo(20, 20);
-    context.lineTo(10, 20);
-    context.closePath();
-    context.stroke();
+    if(mouseDown){
+        context.beginPath();
+        context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+        context.lineTo(e.offsetX, e.offsetY);
+        context.strokeStyle = color;
+        context.stroke();
+        lastEvent = e;
+    }
+}).mouseup(function(){
+    mouseDown = false;
+});
